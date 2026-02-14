2e010e63be9200095d08bd5f4b3b672efe68830a
uvaprol <120366489+uvaprol@users.noreply.github.com>
2026-02-14 12:03:37 +0300

    Create toma_game.py

---
+++ toma_game.py
@@ -0,0 +1,12 @@
+# toma_game
+import pygame
+from Button import Button
+from Tomagochi import Tomagochi
+
+GRAY = (155, 155, 155)
+W = 500
+H = 500
+
+pygame.init()
+DIS = pygame.display.set_mode((W, H))
+pygame.display.set_caption('Томагочи')
